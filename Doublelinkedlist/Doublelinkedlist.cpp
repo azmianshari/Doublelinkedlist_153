7d3e69a45e23dab596fa85696217fff9050102c8
azmianshari <azmianshari123@gmail.com>
2023-05-26 11:07:13 +0700

    Mengaktifkan Doublelinkedlist

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -11,5 +11,30 @@ public:
 };
 
 class Doublelinkedlist {
-
+private:
+	Node* START;
+public:
+	Doublelinkedlist();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
 };
+
+Doublelinkedlist::Doublelinkedlist() {
+	START = NULL;
+}
+
+void Doublelinkedlist::addNode() {
+	int nim;
+	string nama;
+	cout << "\nEnter the roll number of the student :";
+	cin >> nim;
+	cout << "\nEnter the name of student :";
+	cin >> nama;
+	Node* newNode = new Node();
+}
